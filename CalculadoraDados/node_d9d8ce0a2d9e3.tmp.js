0d56db2948060b5d06ad41bf8abc3d477db0b46d
ProsperoAngelo <angeloprospero@hotmail.com>
2024-01-12 15:06:58 -0300

    Primeiro commit

---
+++ CalculadoraDados/node_d9d8ce0a2d9e3.tmp.js
@@ -0,0 +1,23 @@
+let decimal = 25
+let rest
+const binnary = []
+
+for(i = decimal; i > 0; i-i){
+
+    function division(a,b){
+
+        return a >> b
+
+    }
+    
+console.log(decimal)
+
+
+decimal = division(decimal,1)
+
+rest = decimal - division(decimal,1)
+
+binnary.push(rest)
+
+
+}
